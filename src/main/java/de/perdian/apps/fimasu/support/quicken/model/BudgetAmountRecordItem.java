1683a353a997d2afa25f43a008dec36ca5955d7e
Christian Seifert <dev@perdian.de>
2020-01-11 13:51:02 +0100

    Adjust category for title

--- src/main/java/de/perdian/apps/fimasu/support/quicken/model/BudgetAmountRecordItem.java
+++ src/main/java/de/perdian/apps/fimasu/support/quicken/model/BudgetAmountRecordItem.java
@@ -2,7 +2,7 @@ package de.perdian.apps.fimasu.support.quicken.model;
 
 public class BudgetAmountRecordItem extends AbstractStringRecordItem {
 
-    protected BudgetAmountRecordItem(char code, String value) {
+    public BudgetAmountRecordItem(char code, String value) {
         super('B', "0.00|0.00|0.00");
     }
 
--- src/main/java/de/perdian/apps/fimasu/support/quicken/model/SecurityRecordItem.java
+++ src/main/java/de/perdian/apps/fimasu/support/quicken/model/SecurityRecordItem.java
@@ -3,7 +3,7 @@ package de.perdian.apps.fimasu.support.quicken.model;
 public class SecurityRecordItem extends AbstractStringRecordItem {
 
     public SecurityRecordItem(String name) {
-        super('T', name);
+        super('Y', name);
     }
 
 }
