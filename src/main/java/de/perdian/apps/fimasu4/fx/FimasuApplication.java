3d6a91bf254469f1e70958b53434e98920d20ecf
Christian Seifert <dev@perdian.de>
2022-10-23 14:48:26 +0200

    Remove debugging output

--- src/main/java/de/perdian/apps/fimasu4/fx/FimasuApplication.java
+++ src/main/java/de/perdian/apps/fimasu4/fx/FimasuApplication.java
@@ -46,10 +46,6 @@ public class FimasuApplication extends Application {
         FimasuApplicationPane applicationPane = new FimasuApplicationPane(this.getModel(), this.getPreferences());
         Scene applicationScene = new Scene(applicationPane, 1400, 1100);
 
-        applicationScene.focusOwnerProperty().addListener((o, oldValue, newValue) -> {
-            System.err.println("Focus: " + newValue);
-        });
-
         primaryStage.setTitle("FinanzManager Support by perdian");
         primaryStage.setScene(applicationScene);
         primaryStage.setMinWidth(800);
